8c56c12dfbe4c105e1e1196cae1c08a07554e13e
Fernando Costa <fernandocostadev98@gmail.com>
2023-10-17 16:47:46 -0300

    chore: redirect user to home if already authenticated

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,21 @@
 import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export default authMiddleware({
   afterAuth(auth, req, evt) {
-
+    const { pathname } = req.nextUrl;
     if (!auth.userId && !auth.isPublicRoute) {
       return redirectToSignIn({ returnBackUrl: req.url });
     }
+
+    if (pathname == "/" && auth.userId) {
+      const home =  new URL('/home', req.url)
+      return NextResponse.redirect(home);
+    }
   },
-  publicRoutes: ["/"]
+  publicRoutes: ["/"],
 });
 
-
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/(api|trpc)(.*)"],
 };
