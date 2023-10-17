6e976a3ef416c38c4631151ad505007190a74e80
Fernando Costa <fernandocostadev98@gmail.com>
2023-10-17 14:56:55 -0300

    chore: add create bbq validation w/ zod

---
+++ src/lib/forms/bbq/add-bbq.validation.ts
@@ -0,0 +1,28 @@
+import { format } from "date-fns";
+import { z } from "zod";
+
+const required_error = "Campo obrigatório.";
+export const addBBQValidation = z.object({
+  title: z
+    .string({
+      required_error,
+    })
+    .min(5, { message: "Utilize pelo menos 5 caracteres." }),
+  date: z
+    .string({ required_error,invalid_type_error: "Utilize uma data válida." })
+    .transform((date) => format(new Date(date), "yyyy-MM-dd")),
+  hour: z.string({ required_error }),
+  totalPrice: z.string({ required_error }).transform((val) => Number(val)),
+  minContribution: z
+    .string({
+      required_error,
+    })
+    .transform((val) => Number(val)),
+
+  maxCapacity: z
+    .string({ required_error })
+    .min(2)
+    .transform((val) => Number(val)),
+});
+
+export type AddBBQValidation = z.infer<typeof addBBQValidation>;
