80a6d2a9ab98253a1766721b16133fed96d1c12e
Fernando Costa <fernandocostadev98@gmail.com>
2023-10-17 16:30:40 -0300

    fix: add redirectUrl clerk

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 const SigninPage = () => {
   return (
-    <SignIn afterSignInUrl={process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL} />
+    <SignIn afterSignInUrl={process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL} redirectUrl={"/home"} />
   );
 };
 
