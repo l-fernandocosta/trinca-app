7bdab52522950c02b7ab7792f51d9cd42e3aa9bf
Fernando Costa <fernandocostadev98@gmail.com>
2023-10-17 15:22:48 -0300

    fix: remove unused middleware props

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,7 @@
-import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
-
-export default authMiddleware({
-  afterAuth(auth, req, evt) {
-    if (!auth.userId && !auth.isPublicRoute) {
-      return redirectToSignIn({ returnBackUrl: req.url });
-    }
-  },
-  publicRoutes: ["/"]
-});
-
+import { authMiddleware } from "@clerk/nextjs";
+ 
+export default authMiddleware();
+ 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
