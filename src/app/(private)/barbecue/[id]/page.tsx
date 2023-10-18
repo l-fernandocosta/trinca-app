26ce1551e563f92baa6a4ad895ed8ddbd4cc8d14
Fernando Costa <fernandocostadev98@gmail.com>
2023-10-18 06:29:36 -0300

    refactor: add name to barbecue/id page

--- src/app/(private)/barbecue/[id]/page.tsx
+++ src/app/(private)/barbecue/[id]/page.tsx
@@ -1,14 +1,14 @@
 import { BackButton } from "@/components/shared/back-button";
 import { BarbecueProperties } from "./components/barbecue-properties";
 
-const BarbecuePage = async (props : { params: { id: string } }) => {
-  const { id } = props.params;
+const BarbecuePage = async (props : {  params:  {id : string},  searchParams: { origin?: string, name: string}} ) => {
+  const {  name } = props.searchParams;
 
   return (
     <div className='grid grid-cols-1 lg:grid-cols-2 gap-2'>
       <div className='flex flex-row items-center gap-3 w-full border-b py-2 col-span-full'>
         <BackButton />
-        <h1 className='text-sm font-semibold'>{id}</h1>
+        <h1 className='text-sm font-semibold'>{name}</h1>
       </div>
       <BarbecueProperties />
     </div>
